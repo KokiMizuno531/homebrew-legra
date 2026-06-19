d9f34b0bdd46c85b6e8559821be2ee1698a12018
Koki Mizuno <k_mizuno@st.phys.nagoya-u.ac.jp>
2026-06-19 21:46:12 +0900

    Add Legra cask

---
+++ Casks/legra.rb
@@ -0,0 +1,18 @@
+cask "legra" do
+  version "0.1.0"
+  sha256 "3818f7e1b50210f64c8b84db4c034722b5e6d3bd2aad45774a684417cc095c91"
+
+  url "https://github.com/KokiMizuno531/Legra/releases/download/v#{version}/Legra_#{version}_aarch64.app.zip"
+  name "Legra"
+  desc "Local-first paper, PDF, note, and BibTeX manager"
+  homepage "https://github.com/KokiMizuno531/Legra"
+
+  depends_on macos: ">= :big_sur"
+
+  app "Legra.app"
+
+  zap trash: [
+    "~/Library/Application Support/Legra",
+    "~/Library/Application Support/Google/Chrome/NativeMessagingHosts/app.legra.importer.json",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,23 @@
+# Homebrew tap for Legra
+
+Install Legra:
+
+```sh
+brew tap KokiMizuno531/legra
+brew install --cask legra
+```
+
+Upgrade:
+
+```sh
+brew update
+brew upgrade --cask legra
+```
+
+Uninstall:
+
+```sh
+brew uninstall --cask legra
+```
+
+After installing, open Legra, paste the Chrome extension ID in Settings, and click `Install Native Host` to enable Chrome import.
